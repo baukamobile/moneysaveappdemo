9f3c42d1fb8c7d45d6b4dce96aefcc47692ceae1
bauka <newnewernewest123@gmail.com>
2024-04-20 13:23:32 +0500

    до создание новый счетчика трекера

--- lib/widgets/auth/register_page.dart
+++ lib/widgets/auth/register_page.dart
@@ -5,7 +5,7 @@ import 'package:moneyapp/widgets/splitPages.dart';
 import 'package:http/http.dart' as http;
 
 class RegisterPage extends StatelessWidget {
-  String apiUrl = "http://127.0.0.1:8000/api/register/";
+  String apiUrl = "http://172.22.101.26:8000/api/register/";
   
   TextEditingController nameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
@@ -15,7 +15,7 @@ class RegisterPage extends StatelessWidget {
     try {
       final response = await http.post(
         // Uri.parse(apiUrl),
-        Uri.parse("http://10.0.2.2:8000/api/register/"),
+        Uri.parse("http://172.22.101.26:8000/api/register/"),
         body: {
           'name': nameController.text,
           'email': emailController.text,
